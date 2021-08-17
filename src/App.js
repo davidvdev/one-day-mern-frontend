ff6212c0c9c180ea4f2747361f8b57a01a29c222
davidvdev <dvsummercamp@gmail.com>
2021-08-17 14:43:34 -0500

    overdue commit. added basic styling and api calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import {Route, Link, Switch} from 'react-router-dom'
+
+import Body from './components/Body'
 
 function App() {
+
+  const url="https://one-day-mern-dv.herokuapp.com"
+  const [weapons, setWeapons] = useState([])
+
+  const getWeapons = () => {
+    fetch(url + '/swnweapons')
+    .then((response) => response.json())
+    .then((data) => setWeapons(data.data))
+  }
+
+  useEffect(() => {getWeapons()},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Stars Without Number</h1>
+        <h2>unofficial item catalogue</h2>
       </header>
+      <main>
+        <Switch>
+          <Route
+            exact path="/"
+            render={(rp) => (<Body {...rp} weapons={weapons} />)}
+          />
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Body = (props) => {
+    const {weapons} = props
+
+    const loading = () => <h4>Loading Items... This may take a few seconds</h4>
+
+    const loaded = () => {
+        return(
+            <div className="body">
+                {weapons.map(weapon => (
+                    <article key={weapon._id}>
+                        <div className="identifier">
+                            <h2>{weapon.name}</h2>
+                            <img src=
+                                {weapon.type === "melee"
+                                    ? "stiletto.svg"
+                                    : "ray-gun.svg"
+                                }
+                                alt={weapon.name}
+                            />
+                        </div>
+                        <div className="stats">
+                            <h3>Stats</h3>
+                            <ul>
+                                <li>Type: {weapon.type}</li>
+                                <li>Attribute: {weapon.attribute}</li>
+                                <li>Damage: {weapon.damage}</li>
+                                {weapon.type === "melee" ? 
+                                    <li>Shock: {weapon.shock}</li>
+                                    : <><li>Range: {weapon.range}</li><li>Magazine: {weapon.magazine}</li></>}
+                                <li>Encumberance: {weapon.encumberance}</li>
+                                <li>Tech Level: {weapon.TL}</li>
+                                <li>Cost: {weapon.cost}</li>
+                            </ul>
+                        </div>
+                        <div className="description">
+                            <p>{weapon.description}</p>
+                        </div>
+                    </article>
+                ))}
+            </div>
+        )
+    }
+    return weapons.length > 0 ? loaded() : loading();
+}
+
+export default Body
